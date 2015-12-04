17465c7cda66aa1fb101d1ea719f2a291f670f91
Eselta <mathias.jul.jonsson@gmail.com>
2015-12-04 08:56:24 +0100

    Første udkast til klass, mangler nogle

---
+++ src/Medarbejder.java
@@ -0,0 +1,20 @@
+public class Medarbejder{
+	String fornavn;
+	String efternavn;
+	String adresse;
+	String telefonNr;
+	String registreringsNr;
+	String kontoNr;
+	Løntrin lønsats;
+	String stilling;
+
+	public Medarbejder(){
+
+	}
+
+	public void setFornavn(String navn){
+		this.fornavn = navn;
+	}
+
+
+}
